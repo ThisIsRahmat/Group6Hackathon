da08fcb9ba18dd198f9f98e29000aa9cfc39c2ce
Rahmat <thisisrahmat@gmail.com>
2022-10-21 15:55:18 +0100

    Upload audio.py
    So the change made was: to try and refactor the code by removing the non-routing specif parts of the code

--- audio.py
+++ audio.py
@@ -3,31 +3,103 @@ from pyexpat import model
 from flask import Flask, request, url_for, session, render_template, redirect, send_file
 from pytube import YouTube
 from io import BytesIO
-import 
+import openai
 
 # import pixellib
 # from pixellib.instance import instance_segmentation
 import os
 import whisper
 
-model = whisper.load_model("base")
-# summarizer = pipeline("summarization")
+import pytube
+from moviepy.editor import VideoFileClip
+import os
+
+import static_ffmpeg
+
+openai.api_key = os.getenv("OPENAI_API_KEY")
+
+makeup_data = pd.read_csv("products (1).csv")
+brand_products = list(makeup_data['brand_product'].unique())
 
 
-url = input("paste a youtube link: ")
+url = input("Insert a Youtube Link: ")
 
-def get_audio(url):
-  yt = YouTube(url)
-  video = yt.streams.filter(only_audio=True).first()
-  out_file=video.download(output_path=".")
-  base, ext = os.path.splitext(out_file)
-  new_file = base+'.mp3'
-  os.rename(out_file, new_file)
-  a = new_file
-  return a
+def download_video(url):
+    video = pytube.YouTube(url)
+    stream = video.streams.get_by_itag(18)
+    stream.download()
+    return stream.default_filename
+
+def convert_to_mp3(filename):
+    clip = VideoFileClip(filename)
+    clip.audio.write_audiofile(filename[:-4] + ".mp3")
+    clip.close()
+
+def AudiotoText(filename):
+    model = whisper.load_model("base")
+    result = model.transcribe(filename)
+    print(result["text"])
+    sonuc = result["text"]
+    return sonuc
+
+def main(link, model):
+    print('''
+    This tool will convert Youtube videos to mp3 files and then transcribe them to text using Whisper.
+    ''')
+    print("URL: " + link)
+    print("MODEL: " + model)
+    # FFMPEG installed on first use.
+    print("Initializing FFMPEG...")
+    static_ffmpeg.add_paths()
+
+    print("Downloading video... Please wait.")
+    try:
+        filename = download_video(link)
+        print("Downloaded video as " + filename)
+    except:
+        print("Not a valid link..")
+        return
+    try:
+        convert_to_mp3(filename)
+        print("Converted video to mp3")
+    except:
+        print("Error converting video to mp3")
+        return
+    try:
+        mymodel = pywhisper.load_model(model)
+        global result 
+        result = mymodel.transcribe(filename[:-4] + ".mp3")
+        print(result["text"])
+        result = result["text"]
+        os.remove(filename)
+        os.remove(filename[:-4] + ".mp3")
+        print("Removed video and audio files")
+        print("Done!")
+        
+        return result
+    except Exception as e:
+        print("Error transcribing audio to text")
+        print(e)
+        return
 
 
-result = model.transcribe(get_audio(url))
-# return(result['text'])
 
-print(result['text'])
+def gpt3(ai_prompt):
+    entity = openai.Completion.create(
+        input=result,
+        model="text-davinci-002",
+        prompt="Extract cosmetic brands from this text",
+        temperature=0.3,
+        max_tokens=60,
+        top_p=1,
+        frequency_penalty=0.8,
+        presence_penalty=0
+        )
+    return entity
+
+def match_product(entity):
+  matches = {x for x in brand_products if x in entity} #entity is the object been parsed
+  return list(matches) 
+
+if __name__ == "__main__":
+    main()
